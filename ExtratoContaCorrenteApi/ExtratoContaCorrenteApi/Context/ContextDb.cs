01bc9cb5fe5464a6e1381439734cfd691e84d7d0
LucasFerreiraGuedes <lucasguedes01673874@gmail.com>
2023-10-26 11:28:10 -0300

    Criação do modelo de dominio + Configuração do Entity Framework na App

---
+++ ExtratoContaCorrenteApi/ExtratoContaCorrenteApi/Context/ContextDb.cs
@@ -0,0 +1,15 @@
+﻿using ExtratoContaCorrenteApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExtratoContaCorrenteApi.Context
+{
+	public class ContextDb : DbContext
+	{
+		public DbSet<Lancamento> Lancamentos { get; set; }
+
+        public ContextDb(DbContextOptions<ContextDb> options) : base (options)
+        {
+            
+        }
+    }
+}
---
+++ ExtratoContaCorrenteApi/ExtratoContaCorrenteApi/Models/Lancamento.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExtratoContaCorrenteApi.Models
+{
+	  public class Lancamento
+	{
+		[Key]
+        public int Id { get; set; }
+        public string descricao { get; set; }
+        public DateTime data { get; set; }
+        public double valor { get; set; }
+        public Boolean avulso { get; set; }
+        public enum status { Válido, Cancelado }
+
+		public Lancamento(int id, string descricao, double valor, bool avulso)
+		{
+			Id = id;
+			this.descricao = descricao;
+			this.data = DateTime.Now;
+			this.valor = valor;
+			this.avulso = avulso;
+		}
+
+		public Lancamento()
+		{
+
+		}
+	}
+}
